28510a670182b4809518ca44d6ed21dc2b3ddc2c
Your Name <you@example.com>
2026-05-08 17:39:09 +0300

    Update app name to Smart Study Assistant

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,7 +1,7 @@
 import 'secrets.dart';
 
 class AppConstants {
-  static const String appName = 'Smart AI';
+  static const String appName = 'Smart Study Assistant';
   static const String hiveBoxName = 'chat_history';
 
   static String get geminiApiKey => Secrets.geminiApiKey;
