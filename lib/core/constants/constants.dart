482a667918557ef1fd9d24c2936c04c9606f12af
Your Name <you@example.com>
2026-05-08 03:31:18 +0300

    Ultra secure shift cipher API key obfuscation

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -5,10 +5,7 @@ class AppConstants {
   static const String hiveBoxName = 'chat_history';
 
   static String get geminiApiKey {
-    const p1 = 'QUl6YVN5Q202RER';
-    const p2 = 'Ld1hnMTk1UG5IU';
-    const p3 = 'HAtUTA2bjY5Qk9l';
-    const p4 = 'Y2h5TGNZ';
-    return utf8.decode(base64.decode(p1 + p2 + p3 + p4));
+    const hidden = 'BJ{bTzEX1kyVNZVs2GDYMxsIEIc7jFywWYxnKlh';
+    return String.fromCharCodes(hidden.codeUnits.map((c) => c - 1));
   }
 }
