f04745dbddc4d127bcfa08dd1e7823ce65e67fcd
Your Name <you@example.com>
2026-05-08 03:04:56 +0300

    Rename app to Smart AI across all platforms

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,7 +1,7 @@
 import 'secrets.dart';
 
 class AppConstants {
-  static const String appName = 'Smart Study AI';
+  static const String appName = 'Smart AI';
   static String get geminiApiKey => Secrets.geminiApiKey;
   static const String hiveBoxName = 'chat_history';
 }
